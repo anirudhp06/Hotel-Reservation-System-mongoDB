e0278c6ff2cf65dbe9cad5a2a49ea18568400e9f
Anirudh Parvatikar <71212295+anirudhp06@users.noreply.github.com>
2022-08-21 10:43:32 +0530

    First interation with mongoDB

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,21 @@
 package org.example;
+import static com.mongodb.client.model.Filters.eq;
+import org.bson.Document;
+import org.bson.*;
+import org.json.*;
 import com.mongodb.client.*;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String uri = "mongodb://localhost:27017";
+        try(MongoClient client = MongoClients.create(uri)) {
+            MongoDatabase database = client.getDatabase("Testing");
+            MongoCollection<Document> collection = database.getCollection("HelloWorld");
+            System.out.println("Selected Collection 'HelloWorld'");
+            Document doc=collection.find(eq("name", "Anirudh")).first();
+            //System.out.println(doc.toJson());
+            JSONObject obj=new JSONObject(doc.toJson());
+            System.out.println("ID of the document is:"+obj.get("_id"));
+            System.out.println("Name of the document is:"+obj.get("name"));
+        }
     }
 }
