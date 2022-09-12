12d90bdba2b004062c4e006db860c837af2a1b4a
Anirudh Parvatikar <71212295+anirudhp06@users.noreply.github.com>
2022-09-12 12:18:11 +0530

    Connect Remotely 1. Changed location of mongoDB from local to cloud. 2. Added Environment variables to handle mongoDB connection credentials 3. Performed Insertion operation on remote database.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,18 +3,29 @@ import static com.mongodb.client.model.Filters.eq;
 import org.bson.Document;
 import org.bson.*;
 import org.json.*;
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.ServerApi;
+import com.mongodb.ServerApiVersion;
 import com.mongodb.client.*;
+import io.github.cdimascio.dotenv.Dotenv;
 public class Main {
     public static void clearScreen() {  
         System.out.print("\033[H\033[2J");  
         System.out.flush();  
     } 
     public static void main(String[] args) {
-        String uri = "mongodb://localhost:27017";
-        try(MongoClient client = MongoClients.create(uri)) {
+        Dotenv env=Dotenv.load();
+        ConnectionString connectionString= new ConnectionString("mongodb+srv://"+env.get("USERNAMEE")+":"+env.get("PASSWORD")+"@anirudh-cluster.zifgfwn.mongodb.net/?retryWrites=true&w=majority");
+        MongoClientSettings settings=MongoClientSettings.builder()
+        .applyConnectionString(connectionString).serverApi(ServerApi.builder()
+        .version(ServerApiVersion.V1).build())
+        .build();
+        try(MongoClient client = MongoClients.create(settings)) {
             MongoDatabase database = client.getDatabase("Testing");
             MongoCollection<Document> collection = database.getCollection("HelloWorld");
             clearScreen();
+            System.out.println("Loaded Env file");
             System.out.println("Selected Collection 'HelloWorld'");
             Document doc=collection.find(eq("name", "Uday")).first();
             //System.out.println(doc.toJson());
@@ -28,6 +39,8 @@ public class Main {
             .append("lang", "English")
             .append("ethnicity", "Mexican");
             database.getCollection("HelloWorld").insertOne(doc);
+            System.out.println("Added into Collection");
+            System.out.println(env.get("TOKEN"));
         }
     }
 }
