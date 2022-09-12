4dbc3a90e94f000c61201296e880128de0d77f16
Anirudh Parvatikar <71212295+anirudhp06@users.noreply.github.com>
2022-09-12 13:18:14 +0530

    Added Swing frames 1. Frame will now display key value data as Rows & columns.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,4 +1,5 @@
 package org.example;
+
 import static com.mongodb.client.model.Filters.eq;
 import java.util.ArrayList;
 import java.util.List;
@@ -11,6 +12,14 @@ import com.mongodb.ServerApi;
 import com.mongodb.ServerApiVersion;
 import com.mongodb.client.*;
 import io.github.cdimascio.dotenv.Dotenv;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyVetoException;
+import java.beans.VetoableChangeListener;
+import javax.swing.table.DefaultTableModel;
+
 public class Main {
     public static void clearScreen() {  
         System.out.print("\033[H\033[2J");  
@@ -34,14 +43,39 @@ public class Main {
             JSONObject obj=new JSONObject(doc.toJson());
             System.out.println(obj.length()-1);
             List<String> cols=new ArrayList<String>();
+            List<String> rowData=new ArrayList<String>();
             for(String keyStr:obj.keySet()){
                 if(keyStr.equals("_id")){continue;}//Ignores the _id value
                 Object keyVal=obj.get(keyStr);
                 cols.add(keyStr);
+                rowData.add((String)keyVal);
                 System.out.println(keyStr+":"+keyVal);
             }
             System.out.println("Number of columns in this database are(_id ignored):");
             System.out.println(cols);
+            final JFrame frame;
+            JTabbedPane myListTabs = null;
+            frame = new JFrame("Pending Bookings");
+            myListTabs = new JTabbedPane();
+            JTable myComicsTable = null;
+            DefaultTableModel model=new DefaultTableModel();
+            myComicsTable = new JTable(model);
+            myComicsTable.setPreferredScrollableViewportSize(new Dimension(750, 110));
+            myComicsTable.setFillsViewportHeight(true);
+            myComicsTable.setFillsViewportHeight(true);
+            model.setColumnIdentifiers(cols.toArray());
+            //String[] row={"Hello","HI"};
+            model.addRow(rowData.toArray());
+            myComicsTable.setDefaultEditor(Object.class, null);
+            JScrollPane scrollPane = new JScrollPane(myComicsTable);
+            scrollPane.setPreferredSize(new Dimension(600, 110));
+            frame.getContentPane().add(myListTabs);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setAlwaysOnTop(true);
+            frame.pack();
+            frame.setBounds(500, 150, 950, 250);
+            frame.add(scrollPane, BorderLayout.CENTER);
+            frame.setVisible(true);
         }
     }
 }
